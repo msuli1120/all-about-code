eebb3debfdb650157a08cb760099ee17a7c7e6ae
xl <msuli_1120@ymail.com>
2017-02-17 10:44:17 -0800

    add info page, make image clickable and set functions

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$(function(){
+  $("#row1 #row1col1").click(function(event){
+    event.preventDefault();
+    $("#row1col1p").toggle();
+  });
+  $("#row1 #row1col2").click(function(event){
+    event.preventDefault();
+    $("#row1col2p").toggle();
+  });
+  $("#row1 #row1col3").click(function(event){
+    event.preventDefault();
+    $("#row1col3p").toggle();
+  });
+  $("#row1 #row1col4").click(function(event){
+    event.preventDefault();
+    $("#row1col4p").toggle();
+  });
+  $("#row2 #row2col1").click(function(event){
+    event.preventDefault();
+    $("#row2col1p").toggle();
+  });
+  $("#row2 #row2col2").click(function(event){
+    event.preventDefault();
+    $("#row2col2p").toggle();
+  });
+  $("#row2 #row2col3").click(function(event){
+    event.preventDefault();
+    $("#row2col3p").toggle();
+  });
+  $("#row2 #row2col4").click(function(event){
+    event.preventDefault();
+    $("#row2col4p").toggle();
+  });
+});
