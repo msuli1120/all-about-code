20e8d3bb49957482839079de6c0ca98f8a270a0a
xl <msuli_1120@ymail.com>
2017-02-17 14:50:17 -0800

    add functions range age less than 30, 30 to 50 and 50 above

--- js/tryscripts.js
+++ js/tryscripts.js
@@ -7,6 +7,7 @@ $(function(){
 
   $("form#survey").submit(function(event){
     event.preventDefault();
+
     var name = $("form input#usr").val();
     var age = parseInt($("#age").val());
     var design = parseInt($("#gender").val());
@@ -15,10 +16,34 @@ $(function(){
 
     if (design === 1){
       $("#result").text("Hello " + name + ", your suggested stack is " + css + ".");
-    } else if ((design === 2) && (age <= 30) && (gender ==="1")) {
+    } else if ((design === 2) && (age <= 30) && (gender ==="1") && (os === 2)) {
       $("#result").text("Hello " + name + ", your suggested stack is " + java + ".");
-    } else if ((design === 2) && (age <=30) && (gender ==="2")) {
+    } else if ((design === 2) && (age <= 30) && (gender ==="1") && (os === 1)) {
       $("#result").text("Hello " + name + ", your suggested stack is " + csharp + ".");
-    }
+    } else if ((design === 2) && (age <= 30) && (gender ==="1") && (os === 3)) {
+      $("#result").text("Hello " + name + ", your suggested stack is " + ruby + ".");
+    } else if ((design === 2) && (age <= 30) && (gender ==="1") && (os === 4)) {
+      $("#result").text("Hello " + name + ", your suggested stack is " + php + ".");
+    } else if ((design === 2) && (age <=30) && (gender ==="2") && (os === 1)) {
+      $("#result").text("Hello " + name + ", your suggested stack is " + csharp + ".");
+    } else if ((design === 2) && (age <=30) && (gender ==="2") && (os === 2)) {
+      $("#result").text("Hello " + name + ", your suggested stack is " + java + ".");
+    } else if ((design === 2) && (age <=30) && (gender ==="2") && (os === 3)) {
+      $("#result").text("Hello " + name + ", your suggested stack is " + ruby + ".");
+    } else if ((design === 2) && (age <=30) && (gender ==="2") && (os === 4)) {
+      $("#result").text("Hello " + name + ", your suggested stack is " + php + ".");
+    } else if ((design === 2) && ( 30 < age <= 50) && (gender ==="1")) {
+      $("#result").text("Hello " + name + ", your suggested stack is " + php + ".");
+    } else if ((design === 2) && ( 30 < age <= 50) && (gender ==="2")) {
+      $("#result").text("Hello " + name + ", your suggested stack is " + ruby + ".");
+    } else if (((design === 2) &&　(age > 50) && (gender === 2)) || (os === 3)) {
+      $("#result").text("Hello " + name + ", your suggested stack is " + ruby + ".");
+    } else if (((design === 2) &&　(age > 50) && (gender === 1)) || (os === 1)) {
+      $("#result").text("Hello " + name + ", your suggested stack is " + csharp + ".");
+    } else if (((design === 2) &&　(age > 50) && (gender === 2)) || (os ===2)) {
+      $("#result").text("Hello " + name + ", your suggested stack is " + java + ".");
+    } else if (((design === 2) &&　(age > 50) && (gender === 2)) || (os ===4)) {
+      $("#result").text("Hello " + name + ", your suggested stack is " + php + ".");
+    };
   });
 });
